3f36c3d3c45523fcee4de42bf36203b88bb27c50
Pawan H M <25368219+pawanmsr@users.noreply.github.com>
2025-04-05 01:59:26 +0530

    Refactor + Table (#26)

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,2 +1,2 @@
-tocbot.init({tocSelector:".js-toc",contentSelector:".js-toc-content",headingSelector:"h2, h3, h4, h5, h6",hasInnerContainers:!0,scrollSmooth:!1,orderedList:!1}),tocbot.refresh(),MathJax={tex:{inlineMath:[["$","$"],["\\(","\\)"]],tags:"ams"}};
+"undefined"!=typeof tocbot&&(tocbot.init({tocSelector:".js-toc",contentSelector:".js-toc-content",headingSelector:"h2, h3, h4, h5, h6",hasInnerContainers:!0,scrollSmooth:!1,orderedList:!1}),tocbot.refresh()),MathJax={tex:{inlineMath:[["$","$"],["\\(","\\)"]],tags:"ams"}};
 //# sourceMappingURL=post.js.map
